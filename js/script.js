8534ef2f3693acf97843b3a5b917848b26b7d679
SamueleMaka <samuele.maka.imt@gmail.com>
2025-04-30 20:21:29 +0200

    js funziona, ma non la comparsa/scomparsa

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@
 // poi appaiono i campi di imput e spariscono i numeri allo scadere del timer
 
 // ed infine compare la scritta dell'esito della partita una volta mandato invio
+/*
+nascondere roba, fare il controllo, e il reset
+*/
+
+/*
+indicazione
+*/
 
 const numeri = [
     document.getElementById("num-1"),
@@ -11,65 +18,79 @@ const numeri = [
     document.getElementById("num-4"),
     document.getElementById("num-5")
 ]
-
 const output = document.getElementById("punteggio");
 const tempo = document.getElementById("timer");
 const numeriRandom = document.getElementById("numeri");
 const invia = document.getElementById("conferma");
 const inizia = document.getElementById("start");
-const numeriOutput = [];
+const containerNumeri = document.getElementsByClassName("containerNumeri");
+const outputContainer = document.getElementsByClassName("outputContainer");
+const pulsante = document.getElementsByClassName("pulsante");
 
-let interval = null;
-let generatiRandom = [];
 
-invia.addEventListener("click", inviaDati);
+//invia.addEventListener("click", inviaDati);
 inizia.addEventListener("click", start);
+invia.addEventListener("click", inviaDati);
 
-function mostraNumeri(){
-    for(let i=0; i<numeri.length; i++){
+// variabili ed array da usare nel programma
+const generatiRandom = [];
+const numeriUtente = [];
+const soluzioni = [];
+let interval = null;
+
+
+function start(event){
+    // aggiungere un bel reset generale
+    event.preventDefault();
+    timer();
+}
+function generaMostraRandom(){
+    for(let i=0; i<5; i++){
         generatiRandom.push(Math.floor(Math.random() * 100) + 1);
     }
-    numeriRandom.innerHTML = generatiRandom;
+    numeriRandom.innerHTML = generatiRandom.join(",");
 }
-function nascondiNumeri(){
-
-}
-
-
-function inviaDati(event){
-    event.preventDefault();
-    for(let i=0; i<numeri.length; i++){
-        numeriOutput.push(numeri[i].value);
-    }
-    console.log(numeriOutput)
-}
-
-function timer(event){
+function timer(){
     if(interval!== null){
         return;
     }
-    mostraNumeri();
-    let counter = 30;
+    generaMostraRandom();
+    let counter = 5;
     interval = setInterval(function(){
         if(counter === 0){
             tempo.innerHTML = counter;
             clearInterval(interval);
             interval = null;
+            containerNumeri.classList.remove("d-none");
+            containerNumeri.classList.add("d-flex")
+            pulsante.classList.remove("d-none");
+            pulsante.classList.add("d-block")
         }else{
-            tempo.innerHTML = counter;
+            tempo.innerHTML = counter+"s";
             counter--;
         }
     }, 1000);
 }
+function salvaNumeri(){
+    for(let i=0; i<numeri.length; i++){
+        numeriUtente.push(parseInt(numeri[i].value));
+    }
+}
 function controllaNumeri(){
     for(let i=0; i<generatiRandom.length; i++){
-        for(let j=0; j<numeriRandom.length; j++){
-            
+        for(let j=0; j<numeriUtente.length; j++){
+            if(numeriUtente[j] === generatiRandom[i]){
+                soluzioni.push(numeriUtente[j]);
+                break;
+            }
         }
     }
 }
 
-function start(event){
+function inviaDati(event){
     event.preventDefault();
-    timer();
+    salvaNumeri();
+    controllaNumeri();
+    console.log(soluzioni);
+    output.innerHTML = "Hai indovinato: " + soluzioni.join(", ");
 }
